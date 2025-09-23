9ade90c62e5166ac6cc4062c93569aa7e971ea1a
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-09-23 12:06:05 +0300

    Adding data for testing

--- Python-Automation-01/read_excel.py
+++ Python-Automation-01/read_excel.py
@@ -6,6 +6,10 @@ print("Sheet Names Are: ", str(workbook.sheetnames));
 
 sheet = workbook.active
 
+def print_data():
+    for row in sheet.iter_rows(values_only=True):
+        print("The row is: ", row)
+
 print("The value of A1 cell: ", sheet['A1'].value);
 
 print("The cell value is: ", sheet.cell(row=1, column=1).value)
@@ -19,5 +23,11 @@ for value in sheet.iter_rows(min_row=1, max_row=2, min_col=1, max_col=3, values_
 
 for value in sheet.iter_cols(min_row=1, max_row=2, min_col=1, max_col=3, values_only=True):
     print("The values of cols: ", str(value))
+
+sheet["A2"] = "Jafar Loka-02"
+sheet["A3"] = "Jafar Loka-03"
+sheet["B2"] = "Jafar Loka-02"
+
+print_data()
 
 workbook.close();
