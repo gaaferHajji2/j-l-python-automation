493857f45871cd184d9f50c2f0eb512ad2be2606
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-09-23 12:00:21 +0300

    Testing read rows and cols

--- Python-Automation-01/read_excel.py
+++ Python-Automation-01/read_excel.py
@@ -6,8 +6,18 @@ print("Sheet Names Are: ", str(workbook.sheetnames));
 
 sheet = workbook.active
 
-print("The Value of A1 cell: ", sheet['A1'].value);
+print("The value of A1 cell: ", sheet['A1'].value);
 
-print("The Cell Value is: ", sheet.cell(row=1, column=1).value)
+print("The cell value is: ", sheet.cell(row=1, column=1).value)
+
+print("The values using column: ", sheet["A:B"])
+
+print("The values of row 1: ", sheet[1])
+
+for value in sheet.iter_rows(min_row=1, max_row=2, min_col=1, max_col=3, values_only=True):
+    print("The values of rows: ", str(value))
+
+for value in sheet.iter_cols(min_row=1, max_row=2, min_col=1, max_col=3, values_only=True):
+    print("The values of cols: ", str(value))
 
 workbook.close();
