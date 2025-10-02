7fb35483a85d0632014f016333f5adbba96b84f0
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-02 14:25:13 +0300

    extract text in fast way with PyPDF2

--- Python-Automation-01/Normal-Files-Automation/get_pdf_info.py
+++ Python-Automation-01/Normal-Files-Automation/get_pdf_info.py
@@ -4,3 +4,8 @@ pdf = PdfReader('file-01.pdf')
 
 print("Num of pages: ", len(pdf.pages));
 print("info: ", pdf.metadata)
+
+with open('test-pdf-02.txt', 'w', encoding='utf-8') as f:
+    for page in pdf.pages[:207]:
+        text = page.extract_text()
+        f.write(text)
